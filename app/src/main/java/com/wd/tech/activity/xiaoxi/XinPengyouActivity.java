3802a63f2c251b0cbf586f1b71f53d74c828d4af
2318279444@qq.com <2318279444@qq.com>
2020-04-26 13:39:35 +0800

    dxc0426_2

--- app/src/main/java/com/wd/tech/activity/xiaoxi/XinPengyouActivity.java
+++ app/src/main/java/com/wd/tech/activity/xiaoxi/XinPengyouActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.wd.tech.R;
+
 public class XinPengyouActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_xin_pengyou);
+        setContentView(R.layout.activity_lianxiren_key);
     }
 }
--- app/src/main/java/com/wd/tech/activity/xiaoxi/sousuo/LianxirenKeyActivity.java
+++ app/src/main/java/com/wd/tech/activity/xiaoxi/sousuo/LianxirenKeyActivity.java
@@ -3,6 +3,8 @@ package com.wd.tech.activity.xiaoxi.sousuo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wd.tech.R;
+
 public class LianxirenKeyActivity extends AppCompatActivity {
 
     @Override
